b084a55effcd81c10c46b30016c175b4deb726cb
mike <michaelmensah56211@gmail.com>
2023-06-29 01:02:39 +0000

    initial commit

--- app/src/main/java/com/example/picsurfer/screens/home/HomeTopBar.kt
+++ app/src/main/java/com/example/picsurfer/screens/home/HomeTopBar.kt
@@ -1,12 +1,10 @@
 package com.example.picsurfer.screens.home
 
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.picsurfer.ui.theme.topAppBarBackgroundColor
 import com.example.picsurfer.ui.theme.topAppBarContentColor
 
